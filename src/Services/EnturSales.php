058a3f4458d356677f83a07ed39a7ab7f1ab8876
Jørgen Belsaas <jorgen.belsaas@tromsfylke.no>
2024-04-08 15:08:07 +0200

    Issue #2: Added sql key constraints and cvs-import to database

--- src/Services/EnturSales.php
+++ src/Services/EnturSales.php
@@ -2,11 +2,14 @@
 
 namespace Ragnarok\Entur\Services;
 
+use League\Csv\Reader;
 use Ragnarok\Sink\Traits\LogPrintf;
 use Ragnarok\Sink\Services\SinkDisk;
 use Ragnarok\Sink\Models\SinkFile;
 use Ragnarok\Entur\Sinks\SinkEnturSales;
 use Ragnarok\Sink\Services\ChunkArchive;
+use Ragnarok\Sink\Services\ChunkExtractor;
+use Ragnarok\Sink\Services\CsvToTable;
 
 class EnturSales {
     use LogPrintf;
@@ -32,8 +35,65 @@ class EnturSales {
         //return $this->sinkDisk->get("tfk-cleos.csv");
     }
 
-    public function import(string $chunkId, SinkFile $file) {
-        
-        return 0;
+    public function import(string $chunkId, SinkFile $file)
+    {
+        $extractor = new ChunkExtractor(SinkEnturSales::$id, $file);
+        $count = 0;
+        foreach ($extractor->getFiles() as $index => $path) {
+            $this->debug("importing file: %s", $path);
+            $count += $this->importFromCsv($path);
+        }
+        $this->cleanup();
+        return $count;
+    }
+
+    public function destinationTables(): array
+    {
+        return [
+            'entur_product_sales'
+        ];
+    }
+
+    protected function importFromCsv(string $path)
+    {
+        $mapper = new CsvToTable($path, $this->destinationTables()[0], ['SALES_ORDERLINE_ID', 'SALES_FARE_PRODUCT_ID']);
+        $mapper->prepareCsvReader(function (Reader $csv) {
+            $csv->setDelimiter(';');
+        });
+
+        $mapper->column('SALES_ORDERLINE_ID', 'sales_orderline_id');
+        $mapper->column('SALES_FARE_PRODUCT_ID','sales_fare_product_id');
+        $mapper->column('DISTRIBUTION_CHANNEL_REF', 'distribution_channel_ref');
+
+        return $mapper->
+            exec()->
+            logSummary()->
+            getProcessedRecords();
     }
 }
+
+// $table->uuid('sales_orderline_id');
+// $table->uuid('sales_fare_product_id');
+// $table->string('distribution_channel_ref');
+// $table->string('sales_order_id');
+// $table->integer('sales_order_version');
+// $table->string('sales_payment_type');
+// $table->date('sales_date');
+
+// $table->string('sales_package_ref');
+// $table->string('sales_package_name');
+
+// $table->string('sales_user_profile_ref');
+// $table->string('sales_user_profile_name');
+
+// $table->dateTime('sales_start_time');
+
+// $table->string('sales_from_stop_place');
+// $table->string('sales_from_stop_name');
+// $table->string('sales_top_stop_place');
+// $table->string('sales_top_stop_place_name');
+
+// $table->integer('sales_zone_count');
+// $table->string('sales_zones_ref');
+
+// $table->float('annex_amount');

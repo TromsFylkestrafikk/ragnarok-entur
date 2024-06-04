42f2126912380d3b7c97c8cd1569d58ec49a3c7e
Jørgen Belsaas <jorgen.belsaas@tromsfylke.no>
2024-06-04 14:53:23 +0200

    Issue #2: Removed debug comments in config

--- config/ragnarok_entur.php
+++ config/ragnarok_entur.php
@@ -24,18 +24,8 @@ return [
         'client_secret' => env('CLEOS_SECRET'),
         'audience' => env('CLEOS_AUDIENCE'),
         'api_path' => trim(env('CLEOS_API_PATH'), '/'),
-        'env' => env('CLEOS_ENV', 'dev'),
     ],
 
-    /*'cleos' => [
-        'auth_url' => env('ENTUR_AUTH_URL'),
-        'client_id' => env('ENTUR_CLIENT_ID'),
-        'client_secret' => env('ENTUR_SECRET'),
-        'audience' => env('ENTUR_AUDIENCE'),
-        'api_path' => trim(env('ENTUR_API_PATH'), '/'),
-        'env' => env('ENTUR_ENV', 'dev'),
-    ],
-*/
     'routedata' => [
         'main_xml' => env('ENTUR_ROUTEDATA_XML_MAIN', '_Shared_data.xml'),
         'activation_period' => env('ENTUR_ROUTEDATA_PERIOD', 'P30D'),

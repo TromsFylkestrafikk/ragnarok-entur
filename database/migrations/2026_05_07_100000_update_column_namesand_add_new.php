bd767a37c1dd09157ffc444406abd9ffe438031b
Jørgen Belsaas <jorgen.belsaas@tromsfylke.no>
2026-05-19 11:45:14 +0200

    Merged two migrates to one

--- database/migrations/2026_05_07_100000_update_column_namesand_add_new.php
+++ database/migrations/2026_05_07_100000_update_column_namesand_add_new.php
@@ -50,6 +50,31 @@ return new class extends Migration
             $table->renameColumn('line_local_tax_rate', 'acct_local_tax_rate');
             $table->renameColumn('line_tax_amount', 'est_tax_amount');
         });
+
+        Schema::table('entur_product_sales', function (Blueprint $table) {
+            $table->bigInteger('row_id')->after('id')->comment("External row ID");
+            $table->bigInteger('acct_month_id')->after('chunk_id')->comment("Accounting month ID");
+            $table->string('agreement_owner_org_ref')->nullable()->after('acct_month');
+            $table->string('agreement_owner_org_name')->nullable()->after('agreement_owner_org_ref');
+            $table->string('agreement_id')->nullable()->after('agreement_owner_org_name');
+            $table->string('agreement_code')->after('agreement_name');
+            $table->string('distribution_channel_name')->nullable()->after('distribution_channel_ref');
+            $table->string('pos_provider_name')->nullable()->after('pos_provider_ref');
+            $table->string('pos_supplier_name')->nullable()->after('pos_supplier_ref');;
+            $table->string('settlement_number')->nullable()->after('pos_privatecode');
+            $table->string('settlement_external_number')->nullable()->after('settlement_number');
+            $table->string('settlement_external_date')->nullable()->after('settlement_external_number');
+            $table->string('transaction_timestamp')->nullable()->after('transaction_type');
+            $table->string('fare_product_ref')->nullable()->after('fare_product_id');
+            $table->string('fare_product_name')->nullable()->after('fare_product_ref');
+            $table->string('group_json')->nullable()->after('user_profile_name');
+            $table->string('fare_product_authority_ref')->nullable()->after('discount_right_name');
+            $table->string('fare_product_authority_name')->nullable()->after('fare_product_authority_ref');
+            $table->string('usage_validity_ref')->nullable()->after('fare_product_authority_name');
+            $table->string('usage_validity_name')->nullable()->after('usage_validity_ref');
+            $table->string('entitlement_given_ref')->nullable()->after('usage_validity_name');
+            $table->string('entitlement_given_name')->nullable()->after('entitlement_given_ref');
+        });
     }
 
     /**
